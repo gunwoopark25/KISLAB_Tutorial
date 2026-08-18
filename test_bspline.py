1253a2d2e53e40e9d091d571fb7e960e7c27a285
DESKTOP-2R6ACTU\User <gunwoo0122@mail.ulsan.ac.kr>
2026-08-19 00:45:25 +0900

    B-Spline - Class de_boor 생성자 코드 작성 및 디버깅 완료 + Greville abscissae 디버깅 완료

--- test_bspline.py
+++ test_bspline.py
@@ -1,6 +1,9 @@
-from BezierCurve import DeCasteljau
+from BSpline import de_boor
 from Vector import Vector
 
+# ============================================================
+# 데이터 입력
+# ============================================================
 input_data = {
     "cp0": 50,
     "cp1": 175,
@@ -11,3 +14,14 @@ input_data = {
     "Degree": 3,
     "knots" : [50,50,50,200,350,350,350],
 }
+
+bsplinecurve = de_boor(input_data)
+print("control_points =")
+for control_point in bsplinecurve.control_points:
+    print(control_point)
+
+# ============================================================
+# Greville abscissae
+# ============================================================
+abscissae = bsplinecurve.greville()
+print(abscissae)
